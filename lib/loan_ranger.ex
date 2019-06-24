5c12461d831daaa5bee4d908f3f239bf1153504d
Plus17 <sergio.valente@resuelve.mx>
2019-06-24 14:20:01 -0500

    initial project

---
+++ lib/loan_ranger.ex
@@ -0,0 +1,18 @@
+defmodule LoanRanger do
+  @moduledoc """
+  Documentation for LoanRanger.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LoanRanger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/loan_ranger_test.exs
@@ -0,0 +1,8 @@
+defmodule LoanRangerTest do
+  use ExUnit.Case
+  doctest LoanRanger
+
+  test "greets the world" do
+    assert LoanRanger.hello() == :world
+  end
+end
