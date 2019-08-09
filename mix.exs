4157936bb3c2a3ff3e4d21861fcb0dfa17ebb06c
Plus17 <sergio.valente@resuelve.mx>
2019-08-08 23:29:35 -0500

    adds credo and ex_doc dependencies

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LoanRanger.MixProject do
     [
       app: :loan_ranger,
       version: "0.1.0",
-      elixir: "~> 1.9-dev",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,8 +21,8 @@ defmodule LoanRanger.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.14", only: [:dev, :docs]}
     ]
   end
 end
