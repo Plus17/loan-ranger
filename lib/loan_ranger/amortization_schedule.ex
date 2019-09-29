42e8c89a550fc388547a5218458852e892ae189c
Plus17 <sergio.valente@resuelve.mx>
2019-09-28 19:53:32 -0500

    generate amortization schedule

--- lib/loan_ranger/amortization_schedule.ex
+++ lib/loan_ranger/amortization_schedule.ex
@@ -1,16 +1,16 @@
-defmodule LoanRanger.AmortizationTable do
+defmodule LoanRanger.AmortizationSchedule do
   @moduledoc """
-  Logic for Amortization Tables
+  Logic for Amortization Schedule
   """
 
   alias LoanRanger.Loan
+  alias LoanRanger.Amortization
+  alias LoanRanger.Helpers.LoanHelper
 
   defstruct [
-    :loan_amount,
-    :annual_interest_rate,
+    :amortizations,
     :payment_amount,
-    :opening_date,
-    :term,
+    :total_principal_paid
   ]
 
   @doc """
@@ -27,5 +27,85 @@ defmodule LoanRanger.AmortizationTable do
       end)
 
     Enum.reverse(payment_dates_list)
+  end
+
+  @doc """
+  Get amortization schedule
+  """
+  @spec get(Loan.t) :: AmortizationSchedule.t
+  def get(%Loan{opening_date: opening_date, payment_amount: payment_amount, loan_amount: loan_amount, annual_interest_rate: annual_interest_rate} = loan) do
+    payment_dates = generate_payment_dates(loan)
+
+    proccess_amortizations(payment_dates, opening_date, payment_amount, loan_amount, annual_interest_rate, [])
+  end
+  
+  @doc """
+  Process amortizations by payment dates
+  """
+  @spec proccess_amortizations([Date.t], Date.t, Money.t, Money.t, Decimal.t, list) :: [Amortization.t]
+  def proccess_amortizations([payment_date | tail], previous_payment_date, payment_amount, previous_balance, annual_interest_rate, acc) do
+    amortization = calculate_amortization(payment_date, previous_payment_date, payment_amount, previous_balance, annual_interest_rate)
+
+    amortizations = [amortization | acc]
+
+    proccess_amortizations(tail, payment_date, payment_amount, amortization.balance, annual_interest_rate, amortizations)
+  end
+
+  def proccess_amortizations([], _previous_payment_date, _payment_amount, _previous_balance, _annual_interest_rate, amortizations), do: Enum.reverse(amortizations)
+  
+  @doc """
+  Calculates an amortization
+  """
+  @spec calculate_amortization(Date.t, Date.t, Money.t, Money.t, Decimal.t) :: Amortization.t
+  def calculate_amortization(payment_date, previous_payment_date, payment_amount, previous_balance, annual_interest_rate) do
+    daily_interest_rate = _calculates_daily_interest_rate(annual_interest_rate) |> IO.inspect(label: "daily_interest_rate")
+    days_for_interest = _calculates_days_for_interest(payment_date, previous_payment_date) |> IO.inspect(label: "days_for_interest")
+    interest_to_apply = _calculates_interest_to_apply(daily_interest_rate, days_for_interest) |> IO.inspect(label: "interest_to_apply")
+
+    float_interest_to_apply = Decimal.to_float(interest_to_apply)
+
+    interest_amount = _calculates_interest_amount(float_interest_to_apply, previous_balance) |> IO.inspect(label: "INTEREST AMOUNT")
+    principal = Money.subtract(payment_amount, interest_amount) |> IO.inspect(label: "PRINCIPAL")
+
+    %Amortization{
+      date: payment_date,
+      payment_amount: payment_amount,
+      principal: principal,
+      interest: interest_amount,
+      balance: Money.subtract(previous_balance, principal) |> IO.inspect(label: "BALANCE")
+    }
+  end
+  
+  # Calculate interest amount
+  @spec _calculates_interest_amount(Decimal.t, Money.t) :: Money.t
+  defp _calculates_interest_amount(interest_to_apply, previous_balance) do
+    Money.multiply(previous_balance, interest_to_apply)
+  end
+
+  # Calculate daily interest rate
+  @spec _calculates_daily_interest_rate(Decimal.t) :: Decimal.t
+  defp _calculates_daily_interest_rate(annual_interest_rate) do
+    interest_rate = _percentage_to_decimal(annual_interest_rate)
+    Decimal.div(interest_rate, Decimal.new("360"))
+  end
+
+  # Calculate days to collect interest
+  @spec _calculates_days_for_interest(Date.t, Date.t) :: integer
+  defp _calculates_days_for_interest(payment_date, previous_payment_date) do
+    Date.diff(payment_date, previous_payment_date)
+  end
+
+  # Chance decimal percentage to decimal
+  @spec _percentage_to_decimal(Decimal.t()) :: Decimal.t()
+  defp _percentage_to_decimal(annual_interest_rate) do
+    Decimal.div(annual_interest_rate, Decimal.new("100"))
+  end
+  
+  # Calculates interest to apply
+  @spec _calculates_interest_to_apply(Decimal.t(), integer) :: Decimal.t()
+  defp _calculates_interest_to_apply(daily_interest_rate, days_for_interest) do
+    daily_interest_rate
+    |> Decimal.mult(days_for_interest)
+    |> Decimal.round(2)
   end
 end
--- test/loan_ranger/amortization_schedaule_test.exs
+++ test/loan_ranger/amortization_schedaule_test.exs
@@ -2,13 +2,14 @@ defmodule LoanRanger.AmortizationScheduleTest do
   use ExUnit.Case
 
   alias LoanRanger.Loan
-  alias LoanRanger.AmortizationTable
+  alias LoanRanger.Amortization
+  alias LoanRanger.AmortizationSchedule
 
   setup do
     params = %{
       loan_amount: 8_500_000,
       annual_interest_rate: "60.0",
-      payment_amount: 759_500,
+      payment_amount: 727_143,
       opening_date: "2018-12-28",
       term: 18
     }
@@ -41,9 +42,37 @@ defmodule LoanRanger.AmortizationScheduleTest do
         ~D[2020-06-28]
       ]
       
-    generated_list_dates = AmortizationTable.generate_payment_dates(loan)
+    generated_list_dates = AmortizationSchedule.generate_payment_dates(loan)
 
     assert generated_list_dates == expected_list_dates
     assert Enum.count(generated_list_dates) == loan.term
+  end
+
+  test "get/1 returns the right amortization schedule", %{loan: loan} do
+    expected_table = [
+      %Amortization{date: ~D[2019-01-28], payment_amount: Money.new(727_143), principal: Money.new(302_143), interest: Money.new(4250_00), balance: Money.new(8_197_857)},
+      %Amortization{date: ~D[2019-02-28], payment_amount: Money.new(727_143), principal: Money.new(317_250), interest: Money.new(4098_93), balance: Money.new(7_880_607)},
+      %Amortization{date: ~D[2019-03-28], payment_amount: Money.new(727_143), principal: Money.new(333_113), interest: Money.new(3940_30), balance: Money.new(7_547_494)},
+      %Amortization{date: ~D[2019-04-28], payment_amount: Money.new(727_143), principal: Money.new(349_768), interest: Money.new(3773_75), balance: Money.new(7_197_726)},
+      %Amortization{date: ~D[2019-05-28], payment_amount: Money.new(727_143), principal: Money.new(367_257), interest: Money.new(3598_86), balance: Money.new(6_830_469)},
+      %Amortization{date: ~D[2019-06-28], payment_amount: Money.new(727_143), principal: Money.new(385_620), interest: Money.new(3415_23), balance: Money.new(6_444_849)},
+      %Amortization{date: ~D[2019-07-28], payment_amount: Money.new(727_143), principal: Money.new(404_901), interest: Money.new(3222_42), balance: Money.new(6_039_948)},
+      %Amortization{date: ~D[2019-08-28], payment_amount: Money.new(727_143), principal: Money.new(425_146), interest: Money.new(3019_97), balance: Money.new(5_614_802)},
+      %Amortization{date: ~D[2019-09-28], payment_amount: Money.new(727_143), principal: Money.new(446_403), interest: Money.new(2807_40), balance: Money.new(5_168_399)},
+      %Amortization{date: ~D[2019-10-28], payment_amount: Money.new(727_143), principal: Money.new(468_723), interest: Money.new(2584_20), balance: Money.new(4_699_676)},
+      %Amortization{date: ~D[2019-11-28], payment_amount: Money.new(727_143), principal: Money.new(492_159), interest: Money.new(2349_84), balance: Money.new(4_207_517)},
+      %Amortization{date: ~D[2019-12-28], payment_amount: Money.new(727_143), principal: Money.new(516_767), interest: Money.new(2103_76), balance: Money.new(3_690_750)},
+      %Amortization{date: ~D[2020-01-28], payment_amount: Money.new(727_143), principal: Money.new(542_605), interest: Money.new(1845_38), balance: Money.new(3_148_145)},
+      %Amortization{date: ~D[2020-02-28], payment_amount: Money.new(727_143), principal: Money.new(569_736), interest: Money.new(1574_07), balance: Money.new(2_578_409)},
+      %Amortization{date: ~D[2020-03-28], payment_amount: Money.new(727_143), principal: Money.new(598_223), interest: Money.new(1289_20), balance: Money.new(1_980_186)},
+      %Amortization{date: ~D[2020-04-28], payment_amount: Money.new(727_143), principal: Money.new(628_134), interest: Money.new(99_009), balance: Money.new(1_352_052)},
+      %Amortization{date: ~D[2020-05-28], payment_amount: Money.new(727_143), principal: Money.new(659_540), interest: Money.new(67_603), balance: Money.new(692_512)},
+      %Amortization{date: ~D[2020-06-28], payment_amount: Money.new(727_143), principal: Money.new(692_517), interest: Money.new(34_626), balance: Money.new(-5)}
+    ]
+
+    amortization_schedule = AmortizationSchedule.get(loan)
+
+    assert Enum.count(amortization_schedule) == loan.term
+    assert amortization_schedule == expected_table
   end
 end
