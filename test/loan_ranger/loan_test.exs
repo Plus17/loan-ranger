e6608c1d73703cf16fc5157f70c446e51c11a6c1
Plus17 <sergio.valente@resuelve.mx>
2019-10-01 20:49:56 -0500

    add logic to load payments

--- test/loan_ranger/loan_test.exs
+++ test/loan_ranger/loan_test.exs
@@ -4,6 +4,7 @@ defmodule LoanTest do
   use ExUnit.Case
 
   alias LoanRanger.Loan
+  alias LoanRanger.Payment
 
   test "create/1 return a loan structure" do
     params = %{
@@ -21,7 +22,34 @@ defmodule LoanTest do
                 annual_interest_rate: Decimal.new("60.0"),
                 payment_amount: %Money{amount: 759_500, currency: :USD},
                 opening_date: Date.from_iso8601!("2018-12-28"),
-                term: 18
+                term: 18,
+                payments: []
               }}
+  end
+
+  test "load_payments/2" do
+    params = %{
+      loan_amount: 8_500_000,
+      annual_interest_rate: "60.0",
+      payment_amount: 759_500,
+      opening_date: "2018-12-28",
+      term: 18
+    }
+
+    payments = [
+      %{amount: 759_500, date: "2019-01-28"},
+      %{amount: 759_500, date: "2019-02-28"},
+      %{amount: 759_500, date: "2019-03-28"}
+    ]
+
+    assert {:ok, loan} = Loan.create(params)
+
+    {:ok, loan} = Loan.load_payments(loan, payments)
+
+    assert loan.payments == [
+             %Payment{amount: %Money{amount: 759_500, currency: :USD}, date: ~D[2019-01-28]},
+             %Payment{amount: %Money{amount: 759_500, currency: :USD}, date: ~D[2019-02-28]},
+             %Payment{amount: %Money{amount: 759_500, currency: :USD}, date: ~D[2019-03-28]}
+           ]
   end
 end
