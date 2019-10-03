03f0d9bbcf5c5d523255e8e74adcaffe9fa9bd48
Plus17 <sergio.valente@resuelve.mx>
2019-10-02 21:04:28 -0500

    adds function to break down payment in to an amortization

--- lib/loan_ranger/amortization.ex
+++ lib/loan_ranger/amortization.ex
@@ -18,4 +18,32 @@ defmodule LoanRanger.Amortization do
           principal: Money.t(),
           balance: Money.t()
         }
+
+  @doc """
+  Calculates an amortization from payment
+  """
+  @spec break_down_payment(Payment.t(), Money.t(), float) :: Amortization.t()
+  def break_down_payment(
+        payment,
+        previous_balance,
+        interest_to_apply
+      ) do
+    interest_amount = _calculates_interest_amount(interest_to_apply, previous_balance)
+    principal = Money.subtract(payment.amount, interest_amount)
+
+    {:ok,
+     %__MODULE__{
+       date: payment.date,
+       payment_amount: payment.amount,
+       principal: principal,
+       interest: interest_amount,
+       balance: Money.subtract(previous_balance, principal)
+     }}
+  end
+
+  # Calculate interest amount
+  @spec _calculates_interest_amount(float, Money.t()) :: Money.t()
+  defp _calculates_interest_amount(interest_to_apply, previous_balance) do
+    Money.multiply(previous_balance, interest_to_apply)
+  end
 end
