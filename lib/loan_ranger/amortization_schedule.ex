ff4e278dfd236432df56c11c5f46aed335270f80
Sergio <me@sergiovp.dev>
2023-09-13 18:25:19 -0600

    refactor: simplify generate_payment_dates

--- lib/loan_ranger/amortization_schedule.ex
+++ lib/loan_ranger/amortization_schedule.ex
@@ -134,16 +134,9 @@ defmodule LoanRanger.AmortizationSchedule do
   """
   @spec generate_payment_dates(Loan.t()) :: [Date.t()]
   def generate_payment_dates(%Loan{term: term, opening_date: opening_date}) do
-    first_payment_date = Timex.shift(opening_date, months: 1)
-
-    payment_dates_list =
-      Enum.reduce(2..term, [first_payment_date], fn _month, acc ->
-        [previous_payment_date | _tail] = acc
-        next_payment_date = Timex.shift(previous_payment_date, months: 1)
-        [next_payment_date | acc]
-      end)
-
-    Enum.reverse(payment_dates_list)
+    Enum.map(1..term, fn month ->
+      Timex.shift(opening_date, months: month)
+    end)
   end
 
   # Gets interest to apply
